c2d81cf2973da229689df7114463cb15441258b6
RochelRodrigues <142062092+RochelRodrigues@users.noreply.github.com>
2023-08-18 20:58:38 -0400

    Create trigonometria.js

---
+++ trigonometria.js
@@ -0,0 +1,12 @@
+var trigonometria = prompt ("Escolha\n 1.Seno\n 2.Cosseno\3.Tangente";
+var angulo = prompt("Digite o ângulo");
+switch(trigonometria){
+    Case '1':
+         alert(Math.sin(angulo * Math.Pi/180).toFixeld(2));breake;
+    Case '2':
+         alert(Math.cos((angulo * Math.Pi/180).toFixeld(2));breake;
+    Case '3':
+    alert(Math.cos((angulo * Math.Pi/180).toFixeld(2));breake;          
+    default:
+            alert("Opção Inválida");
+}   
